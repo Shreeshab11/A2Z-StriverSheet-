445fae764ff2248e362df873fb7987547508a005
Shreeshab11 <shreeshab11@gmail.com>
2024-07-01 12:50:09 +0530

    first

---
+++ DataTypes.java
@@ -0,0 +1,40 @@
+// <!-- Complete the function dataTypeSize() which takes a string as input and returns the size of the data type represented by the given string in byte unit.
+// Return -1 if the input data type is invalid. -->
+
+
+class Solution {
+    
+    static int dataTypeSize(String str) {
+      
+        
+        if(str.equals("Integer")){
+            
+            return 4;
+        }
+        
+        else if(str.equals("Character")){
+            return 1;
+        }
+        else if(str.equals("Long")){
+            return 8;
+        }
+        
+        else if(str.equals("Float")){
+            return 4;
+        }
+        else if(str.equals("Double")){
+            return 8;
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String []args){
+        
+        Scanner sc= new Scanner(System.in);
+        String str=sc.next();
+        int res=dataTypeSize(str);
+        System.out.println(res);
+        
+    }
+}
